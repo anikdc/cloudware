e3ec8210ba7cec90baa02e4c5dcf5709a425ff49
anikdc <anik29dc@gmail.com>
2025-01-26 13:59:22 +0530

    Upload Working, but has many bugs

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, flash, request, redirect, url_for,render_template
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.secret_key = 'aiwillkillme'
 
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    form=upload_file()
+    return render_template('index.html', form=form)
+
+UPLOAD_FOLDER="C:\\Users\\anik2\\OneDrive\\Documents\\College"
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'rar'}
+app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('home'))
+    return redirect(url_for('home'))
 
 if __name__=='__main__':
     app.run(debug=True)
