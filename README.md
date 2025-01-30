94204e8065f7e14cc576dda807a11368a65dd587
anikdc <anik29dc@gmail.com>
2025-01-30 18:51:14 +0530

    Supports multiple file upload

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 Use your own hardware as cloud storage!
 
 ## Current Progress
+ - [ ] Create an upload progress bar
  - [ ] Figure out how static and templates work better
  - [ ] Learn about POST and GET
- - [ ] Download to local storage
- - [ ] Improve download efficiency
+ - [ ] Improve download efficiency (Fetch)
+ - [ ] Maintain Folder Structure
+ - [x] Multiple file storage
+ - [x] Download to local storage
  - [x] Connect Flask to index.html
  - [x] Create index.html and main.py
  - [x] Create venv and import modules 
--- main.py
+++ main.py
@@ -22,20 +22,18 @@ def allowed_file(filename):
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
+        files = request.files.getlist("file")       
+        if not files:
             flash('No file part')
             return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('home'))
+        for file in files:
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                #Commented out for now: return redirect(url_for('home'))
     return redirect(url_for('home'))
 
 if __name__=='__main__':
